deaefc216fc54dbd23abc3ded933960d89cfbff2
Prop3nguin <Spencer.haley@outlook.com>
2026-06-11 11:46:15 -0500

    v0.03.1 - HomepageDocstring

--- app/routes/languages.py
+++ app/routes/languages.py
@@ -4,6 +4,17 @@ languages_bp = Blueprint("languages", __name__)
 
 @languages_bp.route('/')
 def languages_home():
+    """
+    this route should contain the following sections & features: (Does not include elements initialized in base.html)
+        priority ---
+    - language list: table of all languages in the database, with links to their detail pages
+        - a sub-list of dialects for each language, also linking to their detail pages and adding context for their relationships.
+
+        later additions (QOL features) ---
+    - search bar: allows users to search for languages by name, family, region, etc
+    - filters: dropdowns or checkboxes to filter the language list by various criteria (e.g. language family, number of speakers, etc)
+    - stats: summary of how many languages, dialects, scripts, and glyphs currently shown with filters and search applied.
+    """
     return render_template('languages.html')
 
 @languages_bp.route('/<int:language_id>', methods=['GET', 'POST'])
